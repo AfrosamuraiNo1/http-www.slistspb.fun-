2149c90fcdca82bbf6a56c64e4b0ce337ebc266d
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2022-06-05 11:34:03 +0300

    Первый модульный тест и преобразование url, фиктивное представление

--- functional_tests.py
+++ functional_tests.py
@@ -2,13 +2,13 @@ from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-	'''тест нового посетителя'''
+#	'''тест нового посетителя'''
 
 	def setUp(self):
-		'''установка'''
+#		'''установка'''
 		self.browser = webdriver.Firefox()
 	def tearDown(self):
-		'''демонтаж'''
+#		'''демонтаж'''
 		self.browser.quit()
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		'''тест: можно начать список и получить его позже'''
