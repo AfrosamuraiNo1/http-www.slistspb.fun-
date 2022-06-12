233ccce51e60c623d2e7912c0414d21f1a51a750
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2022-06-12 14:39:12 +0300

    сделать functional_tests приложением, спользовать LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 #	'''тест нового посетителя'''
 
 	def setUp(self):
@@ -12,12 +13,17 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 #		'''демонтаж'''
 		self.browser.quit()
+	def check_for_row_in_list_table(self, row_text):
+#'''подтверждение строки в таблице списка'''
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 	def test_can_start_a_list_and_retrieve_it_later(self):
 #		'''тест: можно начать список и получить его позже'''
 
 		# Эдит слышала про крутое новое онлайн-приложение со списком
 		# неотложных дел. Она решает оценить его домашнюю страницу
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		# Она видит, что заголовок и шапка страницы говорят о списках
 		# неотложных дел
 		self.assertIn('To-Do', self.browser.title)
@@ -35,19 +41,21 @@ class NewVisitorTest(unittest.TestCase):
 		# Когда она нажимает enter, страница обновляется, и теперь страница
 		# содержит "1: Купить павлиньи перья" в качестве элемента списка
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows),"Новый элемент списка не появился в таблице")
+		time.sleep(10)
+		self.check_for_row_in_list_table('1: Купить павлиньи перья')
 		# Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
 		# Она вводит "Сделать мушку из павлиньих перьев"
 		# (Эдит очень методична)
-		self.fail('Закончить тест!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Сделать мушку из павлиньих перьев')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(10)
 		# Страница снова обновляется, и теперь показывает оба элемента ее списка
+		self.check_for_row_in_list_table('1: Купить павлиньи перья')
+		self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 		# Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
 		# сайт сгенерировал для нее уникальный URL-адрес – об этом
 		# выводится небольшой текст с объяснениями.
+		self.fail('Закончить тест!')
 		# Она посещает этот URL-адрес – ее список по-прежнему там.
 		# Удовлетворенна
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
