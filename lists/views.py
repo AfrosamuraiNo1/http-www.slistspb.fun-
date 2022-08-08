a399a5b1238204027d296c6d830ce153030213c4
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2022-08-08 12:48:32 +0300

    Новый URL + представление для добавления в существующие списки. ФТ проходит :-)

--- lists/views.py
+++ lists/views.py
@@ -11,13 +11,18 @@ def home_page(request):
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
 
-def view_list(request):
-#'''представление списка'''
-	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
 #    '''новый список'''
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/trustory/')
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+#'''добавить элемент'''
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_page, name='home'),
-    path('lists/new/', views.new_list, name='new_list'),
-    path('lists/trustory/', views.view_list,name='view_list'),
+    path(r'', views.home_page, name='home'),
+    path(r'lists/new', views.new_list, name='new_list'),
+    path(r'lists/(\d+)/', views.view_list, name='view_list'),
+    path(r'lists/(\d+)/add_item', views.add_item, name='add_item'),
 ]
