ef6d4089d6184a3710a9160a93286cc07e82e479
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2022-08-09 11:52:10 +0300

    Установлен STATIC_ROOT в settings и деактивирован admin

--- superlists/settings.py
+++ superlists/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for superlists project.
 
@@ -31,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
